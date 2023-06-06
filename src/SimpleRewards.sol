3b330647c2ecc2e089c45ae0a26b8e5edca57092
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-06-06 11:29:34 -0500

    add solmate

--- src/SimpleRewards.sol
+++ src/SimpleRewards.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@solmate/tokens/ERC20.sol";
+import "@solmate/utils/SafeTransferLib.sol";
+
 contract SimpleRewards {
+    using 
+    
     
 }
